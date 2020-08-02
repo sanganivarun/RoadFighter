af1d92c6ebf738a5f245a756736bb703793d4960
varunsangani <50577642+varunsangani@users.noreply.github.com>
2020-08-03 00:53:57 +0530

    moving bg

--- RoadFighter 2D/Assets/Scripts/BGMovement.cs
+++ RoadFighter 2D/Assets/Scripts/BGMovement.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class BGMovement : MonoBehaviour
 {
-    
+    public float speed = 1f;
+
+    public float clammpos;
+
+    [HideInInspector] public Vector2 StartPos;
+
+    void Start()
+    {
+        StartPos = transform.localPosition;
+    }
+
+    void FixedUpdate()
+    {
+        if(transform.position.y < -12f)
+        {
+            transform.position = new Vector2(0f, 13f);
+        }
+        else
+        {
+            transform.position = new Vector2(0f, transform.position.y - Time.deltaTime * speed);
+        }
+
+
+    }
 }
