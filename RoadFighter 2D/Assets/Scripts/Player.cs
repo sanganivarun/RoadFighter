5547aaf950f3b47a8a7c504e85d9a399f0401212
varunsangani <50577642+varunsangani@users.noreply.github.com>
2020-08-03 00:14:55 +0530

    player movement and colliders

---
+++ RoadFighter 2D/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    #region INSTANTIATE
+    public static Player player = null;
+
+    void OnEnable()
+    {
+        Player.player = this;
+    }
+    #endregion
+
+    private Rigidbody2D rb;
+
+    private float dirX;
+
+    private float moveSpeed = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
+        rb.velocity = new Vector2(dirX, 0f);
+    }
+
+
+
+
+
+
+
+}
