246140f4870038eed86b6170cc3dc5252a595f84
varunsangani <50577642+varunsangani@users.noreply.github.com>
2020-08-03 01:32:20 +0530

    instantiating collectible

--- RoadFighter 2D/Assets/Scripts/CollectibleSpawnManager.cs
+++ RoadFighter 2D/Assets/Scripts/CollectibleSpawnManager.cs
@@ -6,14 +6,33 @@ public class CollectibleSpawnManager : MonoBehaviour
 {
     public static CollectibleSpawnManager csm = null;
 
+    public GameObject myPrefab;
+
     void OnEnable()
     {
         CollectibleSpawnManager.csm = this;
     }
 
+    public float timer;
 
+    public float maxTimer;
 
+    void Start()
+    {
+        
+    }
 
+    void FixedUpdate()
+    {
+        timer -= Time.deltaTime;
 
+        if(timer < 0)
+        {
+            //spawn
+            Instantiate(myPrefab, gameObject.transform.position, Quaternion.identity);
+            Debug.Log("Timer update");
+            timer = maxTimer;
+        }
+    }
 
 }
